8e2d9271ce25a0b4e491971329bf46bcfe8f8518
muhammadjon100101 <muhammadjon100101@gmail.com>
2021-10-01 09:27:36 +0500

    some change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,9 @@ import { AuthPageComponent } from './auth-page/auth-page.component';
 import { RegistrationPageComponent } from './registration-page/registration-page.component';
 import { AnnouncementPageComponent } from './announcement-page/announcement-page.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-<<<<<<< HEAD
 import { HttpClientModule } from '@angular/common/http';
-=======
 import { SallaryComponent } from './sallary/sallary.component';
 import { HeaderComponent } from './header/header.component';
->>>>>>> 530be65ba32c45896962da468eaa08d4cde20481
 
 @NgModule({
   declarations: [
