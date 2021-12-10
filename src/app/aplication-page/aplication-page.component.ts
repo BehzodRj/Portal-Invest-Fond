b775312e609d40ed601f1e5eb34682ea5d7a9268
muhammadjon100101 <87422446+muhammadjon100101@users.noreply.github.com>
2021-12-10 09:58:24 +0300

    add lots for model

--- src/app/aplication-page/aplication-page.component.ts
+++ src/app/aplication-page/aplication-page.component.ts
@@ -7,38 +7,51 @@ import { FormArray, FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./aplication-page.component.scss']
 })
 export class AplicationPageComponent implements OnInit {
-  formApl!: any
-  fileName!: string
-  show = true
+  // formApl!: any
+  // fileName!: string
+  // show = true
 
-  constructor() {}
+  // constructor() {}
 
-  ngOnInit() {
-    this.formApl = new FormGroup({
-      number: new FormControl(''),
-      name: new FormControl(''),
-      summa: new FormControl(''),
-      lots: new FormArray([])
-    })
+  // ngOnInit() {
+  //   this.formApl = new FormGroup({
+  //     number: new FormControl(''),
+  //     name: new FormControl(''),
+  //     summa: new FormControl(''),
+  //     lots: new FormArray([])
+  //   })
+  // }
+
+  // getFileName(value: any) {
+  //   this.fileName = value.target.files[0].name
+  //   this.show = false
+  //   console.log(value.target.files);
+  // }
+
+  // add() {
+  //   (this.formApl.get('lots') as FormArray).push(new FormGroup( {number: new FormControl(''), name: new FormControl(''), summa: new FormControl('')} ))
+  // }
+
+  // deleted(idx: any) {
+  //   (this.formApl.get('lots') as FormArray).removeAt(idx)
+  // }
+
+  // send() {
+  //   const formAplData = {...this.formApl.value}
+  //   console.log(formAplData);
+  // }
+  page: any
+  showModal= false
+
+  constructor() { }
+
+  ngOnInit() {}
+
+  openModal() {
+    this.showModal = true
   }
 
-  getFileName(value: any) {
-    this.fileName = value.target.files[0].name
-    this.show = false
-    console.log(value.target.files);
+  closeModal() {
+    this.showModal = false
   }
-
-  add() {
-    (this.formApl.get('lots') as FormArray).push(new FormGroup( {number: new FormControl(''), name: new FormControl(''), summa: new FormControl('')} ))
-  }
-
-  deleted(idx: any) {
-    (this.formApl.get('lots') as FormArray).removeAt(idx)
-  }
-
-  send() {
-    const formAplData = {...this.formApl.value}
-    console.log(formAplData);
-  }
-
 }
