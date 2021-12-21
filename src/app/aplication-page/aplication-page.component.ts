aad526af3bebccc2c5f53912bc671bb06d881867
BehzodRj <behzodfarishta@gmail.com>
2021-12-21 08:58:45 +0300

    some changes

--- src/app/aplication-page/aplication-page.component.ts
+++ src/app/aplication-page/aplication-page.component.ts
@@ -10,59 +10,143 @@ import { RequestService } from '../all.service';
 export class AplicationPageComponent implements OnInit {
   page: any
   addLotsForm!: FormGroup
-  addLotsLocalData: any = []
+  editLotsForm!: FormGroup
+  addLotsData: any = []
+  editLotsData: any = []
   AddPartnerData:  any = []
+  editFormData: any
   status: any
   column = false
   showModal = false
+  showEditModal = false
+  numId = 2
 
   constructor(private request: RequestService) { }
 
   ngOnInit() {
     this.addLotsForm = new FormGroup({
-      name: new FormControl(''), 
-      numberof_lots: new FormControl(''), 
+      title: new FormControl(''), 
+      lot_number: new FormControl(''), 
+      total: new FormControl(''), 
+      total_dol: new FormControl(''), 
+      total_euro: new FormControl(''), 
+      vat: new FormControl(''), 
+      vat_dol: new FormControl(''),
+      vat_euro: new FormControl(''),
       discount: new FormControl(''), 
-      NoSsomoni: new FormControl(''), 
-      NoSdollar: new FormControl(''), 
-      NoSeuro: new FormControl(''), 
-      somoni: new FormControl(''), 
-      dollar: new FormControl(''),
-      euro: new FormControl(''),
-      partners: new FormControl(''),
+      discount_dol: new FormControl(''), 
+      discount_euro: new FormControl(''), 
+      name: new FormControl(''),
+      leader: new FormControl(false),
+      Gtotal: new FormControl(''),
+      Gtotal_dol: new FormControl(''),
+      Gtotal_euro: new FormControl(''),
+      Gvat: new FormControl(''),
+      Gvat_dol: new FormControl(''),
+      Gvat_euro: new FormControl(''),
+      Gdiscount: new FormControl(''),
+      Gdiscount_dol: new FormControl(''),
+      Gdiscount_euro: new FormControl('')
+    })
+
+    // EditLotsForm
+    this.editLotsForm = new FormGroup({
+      title: new FormControl(''), 
+      lot_number: new FormControl(''), 
+      total: new FormControl(''),
+      total_dol: new FormControl(''), 
+      total_euro: new FormControl(''), 
+      vat: new FormControl(''), 
+      vat_dol: new FormControl(''),
+      vat_euro: new FormControl(''),
+      discount: new FormControl(''), 
+      discount_dol: new FormControl(''), 
+      discount_euro: new FormControl(''), 
+      name: new FormControl(''),
+      leader: new FormControl(false),
     })
   }
 
-  addP() {
+  addP() {    
     const addLotsFormData = {...this.addLotsForm.value}
-    if(addLotsFormData.partners == '') {
+    if(addLotsFormData.name == '') {
       alert('Заполните поле')
     } else {
-      this.AddPartnerData.push({partners: addLotsFormData.partners})
+      this.AddPartnerData.push({name: addLotsFormData.name, leader: addLotsFormData.leader})
+      this.addLotsForm.controls['name'].reset()
     }
-    
-  }
-
-  check(value: any) {
     
   }
   
-  deletePartner() {}
+  deletePartner(index: number) {
+  this.AddPartnerData.splice(index, 1)
+  }
 
   save() {
     const addLotsFormData = {...this.addLotsForm.value}
-    if(addLotsFormData.name == '' || addLotsFormData.numberof_lots == '' ||  addLotsFormData.discount == '' || addLotsFormData.NoSsomoni == '' || addLotsFormData.somoni == '' ) {
+    if(addLotsFormData.title == '' || addLotsFormData.lot_number == '' || addLotsFormData.total == '' || addLotsFormData.vat == '' ||  addLotsFormData.discount == '' ) {
       alert('Заполните поле')
     } else {
-      this.request.addLotsData.push({name: addLotsFormData.name, numberof_lots: addLotsFormData.numberof_lots, discount: addLotsFormData.discount, NoSsomoni: addLotsFormData.NoSsomoni, NoSdollar: addLotsFormData.NoSdollar, NoSeuro: addLotsFormData.NoSeuro, somoni: addLotsFormData.somoni, dollar: addLotsFormData.dollar, euro: addLotsFormData.euroб, partners: this.AddPartnerData})
-      this.addLotsLocalData = this.request.addLotsData
-      console.log(this.addLotsLocalData);
+      this.addLotsData.push({id: this.numId++, title: addLotsFormData.title, lot_number: addLotsFormData.lot_number, total: addLotsFormData.total, total_dol: addLotsFormData.total_dol, total_euro: addLotsFormData.total_euro, vat: addLotsFormData.vat, vat_dol: addLotsFormData.vat_dol, vat_euro: addLotsFormData.vat_euro, discount: addLotsFormData.discount, discount_dol: addLotsFormData.discount_dol, discount_euro: addLotsFormData.discount_euro, partners: this.AddPartnerData})
+      this.editLotsData = this.addLotsData
       this.showModal = false
+      this.AddPartnerData = [];
+      this.addLotsForm.reset()
     }
   }
 
+  closeEditModal() {
+    this.showEditModal = false
+  }
+
+  editButton(id: number) {
+    this.showEditModal = true
+    this.editFormData = this.editLotsData.filter( (res: any) => res.id == id)[0]
+    this.editLotsForm.patchValue(this.editFormData)
+    this.editLotsForm.controls['name'].reset()
+  }
+
+  deleteEdit(index: number) {
+    this.editFormData.name.splice(index, 1)
+  }
+
+  addEditP() {
+    const editLotsFormData = {...this.editLotsForm.value}
+    console.log(editLotsFormData);
+    if(editLotsFormData.name == '') {
+      alert('Заполните поле')
+    } else {
+      this.editFormData.name.push({name: editLotsFormData.name, leader: editLotsFormData.leader})
+      this.editLotsForm.controls['name'].reset()
+    }
+  }
+
+  cahngeEdit() {
+    console.log(this.editFormData);
+    
+    // let idData = this.addLotsData.filter( (res: any) => res.id == this.editLotsData[0].id)
+    // console.log(idData);
+  }
+
   send() {
-    console.log(this.request.addLotsData);
+    const addLotsFormData = {...this.addLotsForm.value}
+    this.request.postOrderRequests(
+      1,
+      addLotsFormData.Gtotal,
+      addLotsFormData.Gtotal_dol,
+      addLotsFormData.Gtotal_euro,
+      addLotsFormData.Gvat,
+      addLotsFormData.Gvat_dol,
+      addLotsFormData.Gvat_euro,
+      addLotsFormData.Gdiscount,
+      addLotsFormData.Gdiscount_dol,
+      addLotsFormData.Gdiscount_euro,
+      this.addLotsData
+    ).subscribe(response => {
+      console.log(response);
+    }, error => {
+      alert(error.message)
+    })
   }
 
 }
