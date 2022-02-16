af671ed0b8bdd0ffb9b6250941c02711385063f0
BehzodRj <behzodfarishta@gmail.com>
2022-02-16 10:34:58 +0500

    some changes

--- src/app/announcer-file-page/announcer-file-page.component.ts
+++ src/app/announcer-file-page/announcer-file-page.component.ts
@@ -8,14 +8,29 @@ import { RequestService } from '../all.service';
   styleUrls: ['./announcer-file-page.component.scss']
 })
 export class AnnouncerFilePageComponent implements OnInit {
+  pageId!: number
   page: any
   showModal = false
-  fileNames = "Файлы" 
+  fileNames = "Файлы"
   constructor(private route: ActivatedRoute, private request: RequestService) { }
 
   ngOnInit() {
     this.route.params.subscribe( (params: any) => {
-      
+      this.pageId = params.id 
+    })
+    this.request.getAnnouncerFiles().subscribe(response => {
+      console.log(response)
+    }, error => {
+      if(error.status == '401') {
+        this.request.refreshToken().subscribe( (response: any) =>  {
+          localStorage.setItem('access_token', response.access_token)
+          location.reload()
+        }, errorToken => {
+          alert(errorToken.message)
+        })
+      } else {
+        alert(error.message)
+      }
     })
   }
 
